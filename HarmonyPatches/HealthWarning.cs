58fa1f17f4127582a51179f98b24b3b08fc024a9
Loloppe <loloppe2020@gmail.com>
2025-12-25 15:24:19 -0500

    Fix incompatibilities with GottaGoFast.

--- HarmonyPatches/HealthWarning.cs
+++ HarmonyPatches/HealthWarning.cs
@@ -3,13 +3,11 @@ using System.Reflection;
 using HarmonyLib;
 
 namespace Tweaks55.HarmonyPatches {
-	[HarmonyPatch(typeof(HealthWarningScenesTransitionSetupDataSO), nameof(HealthWarningScenesTransitionSetupDataSO.Init))]
+	[HarmonyPatch(typeof(HealthWarningViewController), nameof(HealthWarningViewController.Init))]
 	static class PatchHealthWarning {
-		[HarmonyPriority(int.MinValue)]
-		static void Prefix(ref HealthWarningSceneSetupData healthWarningSceneSetupData) {
-			if(Config.Instance.disableHealthWarning) {
-				healthWarningSceneSetupData.taskCompletionSource.SetResult(true);
-			}
+		static void Postfix(HealthWarningViewController __instance) {
+			if(Config.Instance.disableHealthWarning)
+				__instance.DismissHealthAndSafety();
 		}
 		static Exception Cleanup(MethodBase original, Exception ex) => Plugin.PatchFailed(original, ex);
 	}
