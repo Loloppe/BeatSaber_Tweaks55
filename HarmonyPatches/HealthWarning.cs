7ef58a68612108b9a6ef00ff2c6c586b3213640b
Loloppe <loloppe2020@gmail.com>
2025-12-25 16:11:56 -0500

    This skip properly, although a bit slow.

--- HarmonyPatches/HealthWarning.cs
+++ HarmonyPatches/HealthWarning.cs
@@ -3,11 +3,11 @@ using System.Reflection;
 using HarmonyLib;
 
 namespace Tweaks55.HarmonyPatches {
-	[HarmonyPatch(typeof(HealthWarningViewController), nameof(HealthWarningViewController.Init))]
+	[HarmonyPatch(typeof(HealthWarningViewController), nameof(HealthWarningViewController.DidActivate))]
 	static class PatchHealthWarning {
 		static void Postfix(HealthWarningViewController __instance) {
 			if(Config.Instance.disableHealthWarning)
-				__instance.DismissHealthAndSafety();
+				SharedCoroutineStarter.instance.StartCoroutine(__instance.DismissHealthAndSafety());
 		}
 		static Exception Cleanup(MethodBase original, Exception ex) => Plugin.PatchFailed(original, ex);
 	}
